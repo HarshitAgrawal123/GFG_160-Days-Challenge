3cefdadc537751e7475f97977d28d6353434fe5f
Harshitagrawal <harshitagrawal3105@gmail.com>
2025-04-10 23:12:37 +0530

    add Day 16

---
+++ Day16.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        // Your code here
+        unordered_map<char,int>count;
+        for(char ch : s1){
+            count[ch] += 1;
+        }
+        for(char ch : s2){
+            count[ch] -= 1;
+        }
+       for(auto& pair : count){
+           if(pair.second != 0){
+               return false;
+           }
+       } 
+       return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
