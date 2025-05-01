53757a374a10bcc77a3514dcb63b1d90bd7cf104
Harshitagrawal <harshitagrawal3105@gmail.com>
2025-05-01 20:08:00 +0530

    add day 37

---
+++ Day37.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size();
+        for(int i=0;i<n/2;i++){
+            for(int j=i;j<n-i-1;j++){
+                int temp = mat[i][j];
+                mat[i][j]= mat[j][n-1-i];
+                mat[j][n-1-i]= mat[n-1-i][n-1-j];
+                mat[n-1-i][n-1-j]= mat[n-1-j][i];
+                mat[n-1-j][i]=temp;
+            }
+        }
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(n, 0);
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.rotateby90(matrix);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j)
+                cout << matrix[i][j] << " ";
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
