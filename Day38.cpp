7c99e19ad09215c69eb525f254a723ecd3689993
Harshitagrawal <harshitagrawal3105@gmail.com>
2025-05-02 21:50:25 +0530

    add day 38

---
+++ Day38.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+  bool binarySearch(vector<int> &mat , int target){
+   int n= mat.size();
+       int low = 0, high = n-1;
+       while(low<=high){
+           int mid = (low+high)/2;
+           if(mat[mid]==target)
+           return true;
+          else if(target>mat[mid])
+           low =  mid+1;
+          else
+            high = mid-1;
+       }
+       return false;
+  }
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        // your code here
+       int n = mat.size();
+       for(int i=0;i<n;i++){
+           if(binarySearch(mat[i],x))
+             return true;
+       }
+       return false ;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        int x;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        cin >> x;
+        Solution ob;
+        bool an = ob.matSearch(matrix, x);
+        if (an)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
